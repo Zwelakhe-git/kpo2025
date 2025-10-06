713fae6bc34f6957c10349db096f1c1a0b8aadbc
Zwelakhe-git <zweklakhe.mzwet@gmail.com>
2025-10-06 12:00:13 +0000

    edit konektem:urls, create superuser

--- zvelakeblog/urls.py
+++ zvelakeblog/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/konektem/', include('konektem.urls')),
+    path('konektem/', include('konektem.urls')),
 ]
