ee0626bec255216a11987f5e1ac4e1aa28452d2d
Zwelakhe-git <zweklakhe.mzwet@gmail.com>
2025-10-06 00:03:16 +0000

    changin urls.py

--- zvelakeblog/urls.py
+++ zvelakeblog/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('konektem/', include('konektem.urls')),
+    path('personalprojects-ias7.onrender.com/konektem/', include('konektem.urls')),
 ]
