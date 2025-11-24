383a53370e7b79deff6cd9096eb4478c1da49ee6
root <root@s120187.foxcdn.ru>
2025-11-24 19:25:26 +0000

    removing py folders

--- konektem/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Image)
-admin.site.register(Service)
-admin.site.register(Video)
-admin.site.register(News)
